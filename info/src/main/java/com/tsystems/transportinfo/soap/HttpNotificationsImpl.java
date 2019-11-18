7a09ec7c53500a0a929eeb1c8b319d357ae66d99
Vasily Nikolaev <vsnikalex@gmail.com>
2019-11-18 15:06:26 +0300

    Pass drivers/truck stats to Web Service through http

--- info/src/main/java/com/tsystems/transportinfo/soap/HttpNotificationsImpl.java
+++ info/src/main/java/com/tsystems/transportinfo/soap/HttpNotificationsImpl.java
@@ -4,6 +4,7 @@ import com.tsystems.transportinfo.model.DriversStat;
 import com.tsystems.transportinfo.model.TrucksStat;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.jws.WebService;
 import java.util.logging.Level;
@@ -19,14 +20,22 @@ import java.util.logging.Logger;
 )
 public class HttpNotificationsImpl implements Notifications {
 
+    @Inject
+    private Event<DriversStat> driversStatEvent;
+
+    @Inject
+    private Event<TrucksStat> trucksStatEvent;
+
     @Override
     public void updateDriversStat(DriversStat driversStat) {
         log.info("HTTP drivers stat, total: {}", driversStat.getTotal());
+        driversStatEvent.fire(driversStat);
     }
 
     @Override
     public void updateTrucksStat(TrucksStat trucksStat) {
         log.info("HTTP trucks stat, total: {}", trucksStat.getTotal());
+        trucksStatEvent.fire(trucksStat);
     }
 
 }
--- info/src/main/java/com/tsystems/transportinfo/soap/NotificationsImpl.java
+++ info/src/main/java/com/tsystems/transportinfo/soap/NotificationsImpl.java
@@ -4,12 +4,15 @@ import com.tsystems.transportinfo.model.DriversStat;
 import com.tsystems.transportinfo.model.TrucksStat;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.ejb.Stateless;
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.jws.WebService;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Slf4j
+//@Stateless
 @WebService(
         portName = "NotificationsImplPort",
         serviceName = "NotificationsService",
@@ -19,14 +22,25 @@ import java.util.logging.Logger;
 )
 public class NotificationsImpl implements Notifications {
 
+    @Inject
+    private Event<DriversStat> driversStatEvent;
+
+    @Inject
+    private Event<TrucksStat> trucksStatEvent;
+
     @Override
     public void updateDriversStat(DriversStat driversStat) {
+        log.info("Queue drivers stat, available: {}", driversStat.getAvailable());
+        log.info("Queue drivers stat, driving: {}", driversStat.getDriving());
+        log.info("Queue drivers stat, others: {}", driversStat.getOthers());
         log.info("Queue drivers stat, total: {}", driversStat.getTotal());
+        driversStatEvent.fire(driversStat);
     }
 
     @Override
     public void updateTrucksStat(TrucksStat trucksStat) {
         log.info("Queue trucks stat, total: {}", trucksStat.getTotal());
+        trucksStatEvent.fire(trucksStat);
     }
 
 }
